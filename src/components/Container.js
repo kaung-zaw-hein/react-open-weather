1b87900c61cc932e37e40e25ae79f928655cec50
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-04-27 20:47:44 +0630

    CORS fixed

--- src/components/Container.js
+++ src/components/Container.js
@@ -23,7 +23,7 @@ const Container = () => {
         setloading(true);
           let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=4629514969e5416648023e360603cbec`;
     
-          let res = await fetch(url);
+          let res = await fetch(url,{mode: 'cors'});
           let data = await res.json();
           const { temp, humidity, pressure } = data.main;
           const { main: weatherType } = data.weather[0];
